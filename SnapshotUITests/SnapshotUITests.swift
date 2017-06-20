f86cc38a8988676073ae8b97f4eb891f39aec00f
Heberti Almeida <hebertialmeida@gmail.com>
2017-06-20 13:19:21 -0400

    Added verse screenshot

--- SnapshotUITests/SnapshotUITests.swift
+++ SnapshotUITests/SnapshotUITests.swift
@@ -28,18 +28,32 @@ class SnapshotUITests: XCTestCase {
         SDStatusBarManager.sharedInstance().disableOverrides()
     }
 
-    func testExample() {
+    func testScreenshots() {
         app.buttons["continueWithoutLogin"].tap()
         app.buttons["loginAnonimousOptionYes"].tap()
 
+        // Quarterly
         snapshot("01Quarterly")
+
+        // Lesson
         app.buttons["openLesson"].tap()
         snapshot("02Lesson")
+
+        // Read
         app.buttons["readLesson"].tap()
         snapshot("03Reading")
 
+        // Verse
+        let verseRegex = "(?:\\d|I{1,3})?\\s?\\w{2,}\\.?\\s*\\d{1,}\\:\\d{1,}-?,?\\d{0,2}(?:,\\d{0,2}){0,2}"
+        let verses = app.collectionViews.webViews.staticTexts.matching(NSPredicate(format: "label MATCHES %@", verseRegex))
+        XCTAssert(verses.count > 0, "No verses match with Regex")
+        verses.element(boundBy: 0).tap()
+        snapshot("04Verse")
+        app.buttons["dismissBibleVerse"].tap()
+
+        // Theme
         app.buttons["themeSettings"].tap()
-        snapshot("04Theme")
+        snapshot("05Theme")
         app.children(matching: .window).element(boundBy: 0).children(matching: .other).element(boundBy: 1).tap()
 
         app.navigationBars.buttons.element(boundBy: 0).tap()
@@ -47,7 +61,5 @@ class SnapshotUITests: XCTestCase {
 
         app.buttons["openSettings"].tap()
         app.tables.cells.otherElements.matching(identifier: "logOut").element.tap()
-        
     }
-    
 }
