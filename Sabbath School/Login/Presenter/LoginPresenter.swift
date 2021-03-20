9f5a6b506939e847d661eaa8764577599bfc2b9d
VitalikL <lim.vitaliy@gmail.com>
2021-03-20 00:44:46 -0400

    Fix login actions

--- Sabbath School/Login/Presenter/LoginPresenter.swift
+++ Sabbath School/Login/Presenter/LoginPresenter.swift
@@ -20,27 +20,84 @@
  * THE SOFTWARE.
  */
 
+import Firebase
 import SwiftMessages
+import FacebookCore
+import FacebookLogin
+import GoogleSignIn
 
-class LoginPresenter: LoginPresenterProtocol {
+class LoginPresenter: NSObject, LoginPresenterProtocol {
     var controller: LoginControllerProtocol?
     var wireFrame: LoginWireFrameProtocol?
     var interactor: LoginInteractorInputProtocol?
 
     func configure() {
-        interactor?.configure()
+        // interactor?.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
     }
 
     func loginActionAnonymous() {
-        interactor?.loginAnonymous()
+        // interactor?.loginAnonymous()
+        let alertController = UIAlertController(
+            title: "Login anonymously?".localized(),
+            message: "By logging in anonymously you will not be able to synchronize your data, such as comments and highlights, across devices or after uninstalling application. Are you sure you want to proceed?".localized(),
+            preferredStyle: .alert
+        )
+
+        let no = UIAlertAction(title: "No".localized(), style: .default, handler: nil)
+        no.accessibilityLabel = "loginAnonymousOptionNo"
+
+        let yes = UIAlertAction(title: "Yes".localized(), style: .destructive) { [weak self] (_) in
+            Auth.auth().signInAnonymously(completion: { [weak self] (_, error) in
+                if let error = error {
+                    self?.onError(error)
+                } else {
+                    self?.onSuccess()
+                }
+            })
+        }
+        yes.accessibilityLabel = "loginAnonymousOptionYes"
+
+        alertController.addAction(no)
+        alertController.addAction(yes)
+        alertController.accessibilityLabel = "loginAnonymous"
+
+        (controller as? UIViewController)?.present(alertController, animated: true, completion: nil)
     }
 
     func loginActionFacebook() {
-        interactor?.loginFacebook()
+        // interactor?.loginFacebook()
+        let loginManager = LoginManager()
+        let viewController = controller as? UIViewController
+
+        loginManager.logIn(permissions: [.publicProfile, .email], viewController: viewController) { [weak self] (loginResult) in
+            switch loginResult {
+            case .failed(let error):
+                self?.onError(error)
+            case .cancelled:
+                self?.onError("Cancelled" as? Error)
+            case .success(_, _, let accessToken):
+                let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+                self?.performFirebaseLogin(credential: credential)
+            }
+        }
     }
 
     func loginActionGoogle() {
-        interactor?.loginGoogle()
+        // interactor?.loginGoogle()
+        GIDSignIn.sharedInstance().signIn()
+    }
+
+    func performFirebaseLogin(credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { [weak self] (_, error) in
+            if error != nil {
+                self?.onError(error)
+                return
+            } else {
+                self?.onSuccess()
+            }
+        }
     }
 }
 
@@ -61,5 +118,23 @@ extension LoginPresenter: LoginInteractorOutputProtocol {
         messageView.configureContent(title: "", body: "There was an error during login".localized())
         SwiftMessages.show(config: config, view: messageView)
 
+    }
+}
+
+extension LoginPresenter: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        if let error = error {
+            onError(error)
+            return
+        }
+
+        let authentication = user.authentication
+        let credential = GoogleAuthProvider.credential(withIDToken: (authentication?.idToken)!,
+                                                       accessToken: (authentication?.accessToken)!)
+        performFirebaseLogin(credential: credential)
+    }
+
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
     }
 }
